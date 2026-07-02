cde85a92c31e54d4031df8056786101712fd547d
SriAlgo9 <sricharanmallela7@gmail.com>
2026-07-02 18:39:14 +0200

    Initial commit

---
+++ src/solvers/poisson_matrix.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.sparse import lil_matrix
+
+def poisson_matrix(Nx, Ny, h):
+
+    N = Nx * Ny
+
+    A = lil_matrix((N, N))
+
+    def idx(j, i):
+        return j * Nx + i
+
+    for j in range(Ny):
+        for i in range(Nx):
+
+            p = idx(j, i) #p=row
+
+            A[p, p] = -4.0
+
+            # --------------------------------
+            # x-direction
+            # --------------------------------
+
+            if i == 0:
+
+                # left boundary
+                A[p, idx(j, i + 1)] += 2.0
+
+            elif i == Nx - 1:
+
+                # right boundary
+                A[p, idx(j, i - 1)] += 2.0
+
+            else:
+
+                A[p, idx(j, i - 1)] += 1.0
+                A[p, idx(j, i + 1)] += 1.0
+
+            # --------------------------------
+            # y-direction
+            # --------------------------------
+
+            if j == 0:
+
+                # bottom boundary
+                A[p, idx(j + 1, i)] += 2.0
+
+            elif j == Ny - 1:
+
+                # top boundary
+                A[p, idx(j - 1, i)] += 2.0
+
+            else:
+
+                A[p, idx(j - 1, i)] += 1.0
+                A[p, idx(j + 1, i)] += 1.0
+
+    return A / h**2
+
+
+
+# RHS for Pressure Poisson Equation
+
+def build_rhs(ux_star, uy_star, rho, dt, dx):
+
+    Ny, Nx = ux_star.shape
+
+    b = np.zeros(Nx*Ny)
+
+    for j in range(1, Ny-1):
+        for i in range(1, Nx-1):
+
+            k = j*Nx + i
+
+            b[k] = (rho/dt)*(
+                (ux_star[j,i+1] - ux_star[j,i-1])/(2*dx)
+                +
+                (uy_star[j+1,i] - uy_star[j-1,i])/(2*dx)
+            )
+
+    return b
